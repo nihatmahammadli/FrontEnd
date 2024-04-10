52f8ace6ea67e0a335abb3a03a5edd06ba150797
Nihat <154012759+nihatmahammadli@users.noreply.github.com>
2024-04-10 20:44:33 +0400

    folder upload

--- Homework-19 (js 2)/task-3/app.js
+++ Homework-19 (js 2)/task-3/app.js
@@ -1,19 +1,14 @@
 var first = prompt("Your Name please:");
-var firstadd = `1) ${first}`;
-console.log(firstadd);
-
 var second = prompt("Your Surname please:");
-var secondadd = `2) ${second}`;
-console.log(secondadd);
-
 var third = prompt("Your age please:");
-var thirdadd =  `3) ${third}`;
-console.log(thirdadd);
-
 var fourth = prompt("Are you robot? Yes or no");
-var fourthadd = `4) ${fourth}`;
-console.log(fourthadd);
-
 var fifth = prompt ("Are you sure? Yes or no");
-var fifthadd = `5) ${fifth}`;
-console.log(fifthadd);
+
+var add = `
+1) ${first}
+2) ${second}
+3) ${third}
+4) ${fourth}
+5) ${fifth}`;
+
+console.log(add);
